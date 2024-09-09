5ef74788174acddb53711972626cf44cb289bc73
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-09-09 10:54:27 -0400

    starting on removing find item tasks

--- src/mod.ts
+++ src/mod.ts
@@ -3,7 +3,7 @@ import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
-import { IQuestCondition, VisibilityCondition } from "@spt/models/eft/common/tables/IQuest";
+import { IQuest, IQuestCondition, VisibilityCondition } from "@spt/models/eft/common/tables/IQuest";
 import { VFS } from "@spt/utils/VFS";
 import path from "path";
 import { SeededRandom } from "./SeededRandom";
@@ -27,6 +27,8 @@ export class StartCollectorEarly implements IPostDBLoadMod {
         this.logger = container.resolve<ILogger>("WinstonLogger");
         const vfs = container.resolve<VFS>("VFS");
         this.config = jsonc.parse(vfs.readFile(path.resolve(__dirname, "../config/config.jsonc")));
+
+        const quest: IQuest = tables.templates.quests["5c51aac186f77432ea65c552"];
 
         const seededRandom: SeededRandom = new SeededRandom(1);
 
@@ -75,6 +77,12 @@ export class StartCollectorEarly implements IPostDBLoadMod {
                 tables.templates.quests["5c51aac186f77432ea65c552"].conditions.AvailableForFinish.push(conditions[i]);
             }
         }
+
+        // Remove FindItem conditions from available for finish tasks
+        for (let i = quest.conditions.AvailableForFinish; i > 0; i++) {
+            const element = array[i];
+            
+        }
         
         // Modify quest start condition to only need level 1
         const startCondition: IQuestCondition = {
@@ -85,7 +93,7 @@ export class StartCollectorEarly implements IPostDBLoadMod {
             compareMethod: ">=",
             value: this.config.startingLevel
         };
-        tables.templates.quests["5c51aac186f77432ea65c552"].conditions.AvailableForStart = [startCondition];
+        quest.conditions.AvailableForStart = [startCondition];
 
         this.logger.logWithColor("[Start Collector Early] Modified Quest!", LogTextColor.CYAN);
     }
