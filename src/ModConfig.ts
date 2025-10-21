6e3bf5edf859f7ffb26f2ee21d1adc8b97a9bb68
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-10-21 14:45:48 -0400

    Clean slate

--- src/ModConfig.ts
+++
@@ -1,7 +0,0 @@
-export interface ModConfig {
-    startingLevel: number;
-    hideFindItemTasks: boolean;
-    removeFoundInRaidRequirement: boolean;
-    prerequisiteQuestCompletionRequired: boolean;
-    prerequisiteQuestCompletionVisibility: boolean;
-}
